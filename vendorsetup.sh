69f34650ff4ba423749e5a6d4ec6ff800827d91b
Eduzul123 <eduardopuentes630@gmail.com>
2025-02-06 19:22:23 -0400

    Add files via upload

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,10 @@
-add_lunch_combo omni_VIT_T4310-eng
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_VIT_T4310-user
 add_lunch_combo omni_VIT_T4310-userdebug
+add_lunch_combo omni_VIT_T4310-eng
